1b6ccc3b3305f17f380e9ed5437502cabc8c23af
s4ndyp <96690821+s4ndyp@users.noreply.github.com>
2025-12-30 21:06:56 +0100

    Delete src directory

--- src/main.ts
+++
@@ -1,5 +0,0 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { App } from './app/app.component'; // Importeer de App class uit de nieuwe locatie
-
-bootstrapApplication(App)
-  .catch((err) => console.error(err));
